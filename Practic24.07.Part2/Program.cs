f35794022ecb02ff247c5f09ebd6d0cb49e59363
ApxNord <dronov_a@t.top-academy.ru>
2024-07-24 20:57:39 +0300

    Добавьте файлы проекта.

---
+++ Practic24.07.Part2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic24._07.Part2
+{
+    /*
+     Создайте приложение, которое отображает количество
+     чётных, нечётных, уникальных элементов массива.
+    */
+    internal class Program
+    {
+        const int SIZE_ARRAY = 10;
+        static int[] numbers;
+        static void Main(string[] args)
+        {
+            InitArrayNumbers();
+            PrintArrayNumbers();
+
+            Console.WriteLine();
+
+            /*Console.WriteLine($"кол-во четных чисел: {GetCountEvenNumbers()}");
+            Console.WriteLine($"кол-во нечетных чисел: {GetCountOddNumbers()}");
+            Console.WriteLine($"кол-во уникальных чисел: {GetCountUniqueNumbers()}");*/
+            //Console.WriteLine($"кол-во четных чисел: {numbers.Count(n=> n%2 ==0 )}");
+            //Console.WriteLine($"кол-во нечетных чисел: {numbers.Count(n => n % 2 != 0)}");
+            //Console.WriteLine($"кол-во уникальных чисел: {numbers.Distinct().Count()}");
+        }
+
+        static void InitArrayNumbers()
+        {
+            numbers = new int[SIZE_ARRAY];
+            Random rand = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(1, 10);
+            }
+        }
+        static void PrintArrayNumbers()
+        {
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static int GetCountEvenNumbers()
+        {
+            int evenCount = 0;
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 0) evenCount++;
+            }
+            return evenCount;
+        }
+        static int GetCountOddNumbers()
+        {
+            int oddCount = 0;
+            foreach (int i in numbers)
+            {
+                if (i % 2 != 0) oddCount++;
+            }
+            return oddCount;
+        }
+        static int GetCountUniqueNumbers()
+        {
+            int uniqueCount = 0;
+            bool isUnique;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                isUnique = true;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j] && i != j)
+                    {
+                        isUnique = false;
+                    }
+                }
+                if (isUnique)
+                    uniqueCount++;
+            }
+            return uniqueCount;
+        }
+    }
+
+    /*
+     Создайте приложение, отображающее количество
+    значений в массиве меньше заданного параметра пользователем. 
+    Например, количество значений меньших, чем 7
+    (7 введено пользователем с клавиатуры).
+    */
+    /*internal class Program
+    {
+        const int SIZE_ARRAY = 10;
+        static int[] numbers;
+        static void Main(string[] args)
+        {
+            InitArrayNumbers();
+            PrintArrayNumbers();
+
+            int lessNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetCountLessInNumbers(lessNumber));
+        }
+        static void PrintArrayNumbers()
+        {
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static void InitArrayNumbers()
+        {
+            numbers = new int[SIZE_ARRAY];
+            Random rand = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(1, 10);
+            }
+        }   
+        static int GetCountLessInNumbers(int lessNumber)
+        {
+            int count = 0;
+            foreach (int i in numbers) {
+                if (i < lessNumber) count++;
+            }
+            return count;
+        }
+    }*/
+}
---
+++ Practic24.07/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic24._07
+{
+    internal class Program
+    {
+        enum Month
+        {
+            JANUARY = 1,
+            FEBRUARY,
+            MARCH,
+            APRIL,
+            MAY,
+            JUNE,
+            JULE,
+            AUGUST,
+            SEPTEMBER,
+            OCTOBER,
+            NOVEMBER,
+            DECEMBER
+        };
+        enum Season
+        {
+            WINTER,
+            SPRING,
+            SUMMER,
+            AUTUMN
+        };
+
+        enum TEMPERATURE_CONVERT { CELSUIS, FAHRENHEIT};
+        static void Main(string[] args)
+        {
+            /*
+             Пользователь вводит с клавиатуры дату. 
+            Приложение должно отобразить название сезона и дня недели.
+                Например, если введено 22.12.2021, приложение должно
+                отобразить Winter Wednesday.
+             */
+            /*string[] seasons = { "Winter", "Spring", "Summer", "Autumn" };
+            
+            Console.Write("Введите дату -> ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine(date.ToLongDateString());
+            Console.WriteLine(date.ToShortDateString());
+            int seasonIndex = GetSeason(date.Month);
+            Console.WriteLine($"{seasons[seasonIndex]} {date.DayOfWeek}");*/
+
+
+            /*
+             Пользователь вводит с клавиатуры показания температуры. 
+            В зависимости от выбора пользователя программа переводит 
+            температуру из Фаренгейта в Цельсий или наоборот.
+             */
+            Console.WriteLine("Выберете конвертор 1-Из C в F 2- Из F в C");
+            int choise = int.Parse(Console.ReadLine());
+            double temperatureCelsius, temperatureFahrenheit;
+
+            Console.WriteLine(" введите показатель температуры");
+            double temperature = double.Parse(Console.ReadLine());
+            if (choise == (int)TEMPERATURE_CONVERT.FAHRENHEIT)
+            {
+                Console.WriteLine(" конвертор 1-Из C в F");
+                temperatureFahrenheit = FromFahrenheitToCelsius(temperature);
+                Console.WriteLine(temperatureFahrenheit); Console.WriteLine("C");
+            }
+            else if (choise == (int)TEMPERATURE_CONVERT.CELSUIS)
+            {
+                Console.WriteLine(" конвертор 2-Из F в C");                         
+                temperatureCelsius = FromCelsiusToFahrenheit(temperature);
+                Console.WriteLine(temperatureCelsius); Console.WriteLine("F");
+            }
+            else
+            {
+                Console.WriteLine(" ошибка");
+            }
+        }
+        static int GetSeason(int month)
+        {
+            switch (month)
+            {
+                case (int)Month.JANUARY:
+                case (int)Month.FEBRUARY:
+                case (int)Month.DECEMBER:
+                    return (int)Season.WINTER;
+                case (int)Month.MARCH:
+                case (int)Month.APRIL:
+                case (int)Month.MAY:
+                    return (int)Season.SPRING;
+                case (int)Month.JUNE:
+                case (int)Month.JULE:
+                case (int)Month.AUGUST:
+                    return (int)Season.SUMMER;
+                case (int)Month.OCTOBER:
+                case (int)Month.NOVEMBER:
+                case (int)Month.SEPTEMBER:
+                    return (int)Season.AUTUMN;
+                default:
+                    return -1;
+            }
+        }
+
+        static double FromFahrenheitToCelsius(double temperatureFahrenheit)
+        {
+            return (temperatureFahrenheit * 9 / 5) + 32;
+        }
+        static double FromCelsiusToFahrenheit (double temperatureCelius)
+        {
+            return (temperatureCelius + 32) * 5 / 9;
+        }
+    }
+}
